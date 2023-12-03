3fa9c707a72aad76dccd6fb5cc0459c8187085fe
jsheriff20 <jackasheriff@hotmail.co.uk>
2023-12-03 20:19:42 +0000

    refactor repo to be none year specific

---
+++ 2015/day2/part1.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+
+;(() => {
+    let total = 0
+
+    const data = fs.readFileSync("data.txt", "utf8").split("\r\n")
+
+    data.forEach((line) => {
+        const measurements = line.split("x").map((measurement) => parseInt(measurement, 10))
+
+        const calculations = [
+            measurements[0] * measurements[1] * 2,
+            measurements[0] * measurements[2] * 2,
+            measurements[1] * measurements[2] * 2,
+        ]
+
+        let lowestCalculation = calculations[0]
+        calculations.forEach((measurement) => {
+            if (measurement < lowestCalculation) lowestCalculation = measurement
+        })
+
+        const singlePresent = calculations[0] + calculations[1] + calculations[2] + lowestCalculation / 2
+
+        total += singlePresent
+    })
+
+    console.log(total)
+})()
