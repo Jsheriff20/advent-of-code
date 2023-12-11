b310c8901160529618122110c5766893c0c88b4d
Jsheriff20 <jackasheriff@hotmail.co.uk>
2023-12-11 18:02:49 +0000

    solution for 2023 day 9 part 1

---
+++ 2023/day9/part1.js
@@ -0,0 +1,54 @@
+const fs = require("fs")
+
+;(() => {
+    let total = 0
+    const data = fs.readFileSync("data.txt", "utf8").split("\n")
+
+
+    data.forEach(line => {
+        console.log('---')
+        const test = line.split(' ').map(value => parseInt(value))[line.split(' ').map(value => parseInt(value)).length - 1]
+        const extrapolatedRows = [line.split(' ').map(value => parseInt(value))]
+        // extrapolatedRows[0].pop()
+        let i = 0
+
+        do {
+            const currentResults = extrapolatedRows[i]
+            let extrapolatedRow = []
+            for(let j = 0; j < currentResults.length - 1; j++){
+                extrapolatedRow.push(currentResults[j + 1] - currentResults[j])
+            }   
+
+            extrapolatedRows.push(extrapolatedRow)
+            i++
+        } while (extrapolatedRows[extrapolatedRows.length - 1].filter(result => result !== 0).length);
+        
+
+        const extrapolatedResults = []
+
+
+        do {
+            const currentRow = extrapolatedRows[i]
+            const prevRow = extrapolatedResults[extrapolatedResults.length - 1]
+            let extrapolatedResult = currentRow
+
+            if(currentRow.filter(result => result === 0).length == currentRow.length){
+                extrapolatedResult = [...extrapolatedResult, 0]
+
+            }
+            else{
+                const nextValue = currentRow[currentRow.length - 1] + prevRow[prevRow.length -  1]
+                extrapolatedResult.push(nextValue)
+
+                if(i === 0 && nextValue != test) console.log(test)
+                if(i === 0) total += nextValue
+            }
+
+            extrapolatedResults.push(extrapolatedResult)
+            i--
+
+        } while (i >= 0);
+    })
+
+    console.log(total)
+})()
